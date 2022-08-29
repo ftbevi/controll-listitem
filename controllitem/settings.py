785cde8396d9f3fea4aec85ed08aeae8f08d29cd
Thiago Beviláqua <thiago.bevilaqua@grupomateus.com>
2022-08-29 19:33:29 -0300

    add rest framework and drf yasg.

--- controllitem/settings.py
+++ controllitem/settings.py
@@ -28,6 +28,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # third party libs
+    "rest_framework",
+    'drf_yasg',
     # system apps
     'controllitem.store',
 ]
